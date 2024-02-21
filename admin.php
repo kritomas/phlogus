c55cd680958917b0810855d532b88ad7827f4f5e
kritomas <Unknown>
2024-02-21 08:00:36 +0100

    Admin page.

--- admin.php
+++ admin.php
@@ -8,9 +8,14 @@
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
-		<title>Sudo</title>
+		<title>Microsoft Windows Control Panel</title>
 	</head>
 	<body>
-		<h1>Welcome to <b>sudo</b></h1>
+		<h1>Welcome to <b>Microsoft Windows Control PanelTM</b></h1>
+		<button>Collect personal data</button>
+		<button>Collect more personal data</button>
+		<button>Collect Everything</button>
+		<button>Violate privacy</button>
+		<button>Engage protocol NTEF</button> <!--NineTeen Eighty-Four-->
 	</body>
 </html>
