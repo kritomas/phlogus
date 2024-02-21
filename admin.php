0db4c7e041011f8afb3a1d10ca325757dd7bdc58
kritomas <Unknown>
2024-02-21 07:56:03 +0100

    Admin login.

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+
+	//echo ("Welcome to <b>sudo</b>");
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Sudo</title>
+	</head>
+	<body>
+		<h1>Welcome to <b>sudo</b></h1>
+	</body>
+</html>
--- login.php
+++ login.php
@@ -8,8 +8,11 @@
 	}
 
 	$username = $_POST["username"];
+	$password = $_POST["password"];
 	$_SESSION["username"] = $username;
 
-	echo("Yo r logged in as $username");
-	header("Location: welcome.php");
+	$json = get_object_vars(json_decode(file_get_contents("admin.json")));
+	var_dump($json);
+	if ($json["username"] === $username && $json["password"] === $password) header("Location: admin.php");
+	else header("Location: welcome.php");
 ?>
